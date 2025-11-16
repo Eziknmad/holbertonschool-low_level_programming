1b29a1ff77fef44751981d7ccf4f61268afc7651
Eziknmad <galarzaarzon@gmail.com>
2025-11-15 20:44:31 -0400

    Add task 0: sum_them_all function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,21 +11,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list numbers;
-    unsigned int i;
-    int sum = 0;
+	va_list numbers;
+	unsigned int i;
+	int sum = 0;
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    va_start(numbers, n);
+	va_start(numbers, n);
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(numbers, int);
-    }
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
 
-    va_end(numbers);
+	va_end(numbers);
 
-    return (sum);
+	return (sum);
 }
