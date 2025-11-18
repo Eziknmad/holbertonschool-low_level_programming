5790f33d379064c773871699070d35544453d364
Eziknmad <galarzaarzon@gmail.com>
2025-11-17 23:55:36 -0400

    Add task 1: print_numbers variadic function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,12 +3,16 @@
 #include <stdarg.h>
 
 /**
-* print_number - prints numbers separated by a string
-* @separator: string printed between numbers
-* @n: number of integers passed to the function
-*
-* Return: void
-*/
+ * print_numbers - prints numbers separated by a string
+ * @separator: the string printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Description: Prints all the numbers passed after n separated by separator.
+ * If separator is NULL, it is not printed.
+ * Prints a new line at the end.
+ *
+ * Return: void
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
