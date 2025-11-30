fd6fd1cb06b1fc5d0ccaa45a53860be03c9d68a9
Eziknmad <galarzaarzon@gmail.com>
2025-11-29 23:03:46 -0400

    Add cp program that copies file content with error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,76 +1,97 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+void close_file(int fd, char *file_to);
+void check_args(int argc, char *argv[]);
 
 /**
- * check_args - Checks if correct number of arguments is passed.
- * @argc: Number of arguments
- */
-void check_args(int argc)
+* main - Copies the content of a file to another file.
+* @argc: The number of arguments.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success, or exits with specific codes on failure.
+*/
+int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+int fd_from, fd_to, rd, wr;
+char buffer[BUF_SIZE];
+
+check_args(argc, argv); /* Check arguments count */
+
+/* Open the source file */
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+/* Open the destination file */
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to < 0)
+{
+close_file(fd_from, argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+/* Read from source and write to destination */
+while ((rd = read(fd_from, buffer, BUF_SIZE)) > 0)
+{
+wr = write(fd_to, buffer, rd);
+if (wr != rd)
+{
+close_file(fd_from, argv[1]);
+close_file(fd_to, argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+
+if (rd < 0)
+{
+close_file(fd_from, argv[1]);
+close_file(fd_to, argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+close_file(fd_from, argv[1]);
+close_file(fd_to, argv[2]);
+return (0);
 }
 
 /**
- * close_file - Closes a file descriptor, handles errors.
- * @fd: File descriptor to close
- */
-void close_file(int fd)
+* close_file - Closes a file descriptor and handles errors.
+* @fd: The file descriptor to close.
+* @file_to: The name of the file associated with the file descriptor.
+*/
+void close_file(int fd, char *file_to)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+(void)file_to;
+if (close(fd) < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
 }
 
 /**
- * copy_file - Copies content from file_from to file_to.
- * @file_from: Source filename
- * @file_to: Destination filename
+ * check_args - Checks the number of arguments and handles errors.
+ * @argc: The number of arguments.
+ * @argv: An array of pointers to the arguments.
  */
-void copy_file(char *file_from, char *file_to)
+void check_args(int argc, char *argv[])
 {
-	int fd_from, fd_to, r_bytes, w_bytes;
-	char buffer[BUF_SIZE];
+(void)argv; /*Suppress unused parameter warning*/
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close_file(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
-	{
-		w_bytes = write(fd_to, buffer, r_bytes);
-		if (w_bytes != r_bytes)
-		{
-			close_file(fd_from);
-			close_file(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-
-	if (r_bytes == -1)
-	{
-		close_file(fd_from);
-		close_file(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	close_file(fd_from);
-	close_file(fd_to);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 }
--- file_io/main.h
+++ file_io/main.h
@@ -12,8 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_file(int fd);
-void check_args(int argc);
-void copy_file(char *file_from, char *file_to);
+int _putchar(char c);
 
 #endif /* MAIN_H */
